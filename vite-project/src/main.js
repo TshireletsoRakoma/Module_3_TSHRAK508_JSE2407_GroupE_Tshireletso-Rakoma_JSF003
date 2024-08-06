87e0d4d96c69d4857d8850f9f1f097693ffa30a6
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-08-06 11:32:41 +0200

    added the store.js and the installed npm veux

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store'; // Import Vuex store
+import router from './router'; // Import Vue Router
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(store); // Use Vuex store
+app.use(router); // Use Vue Router
+
+app.mount('#app');
